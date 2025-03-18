d9230b2d1c55816faaeb9ff661c13670c2167185
David <davidalejandrolerea@outlook.com>
2025-03-18 20:14:14 -0300

    arreglos

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import { TeacherForm } from './components/TeacherForm';
 import { Dashboard } from './components/Dashboard';
 import { SubjectList } from './components/SubjectList';
@@ -9,8 +9,16 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Dashboard />} />
+        {/* Redirigir la raíz "/" a "/nuevo" automáticamente */}
+        <Route path="/" element={<Navigate to="/nuevo" replace />} />
+        
+        {/* Página de creación de horarios */}
         <Route path="/nuevo" element={<TeacherForm />} />
+
+        {/* Dashboard con los horarios */}
+        <Route path="/dashboard" element={<Dashboard />} />
+
+        {/* Gestión de materias */}
         <Route path="/materias" element={<SubjectList />} />
         <Route path="/materias/nueva" element={<SubjectForm />} />
       </Routes>
